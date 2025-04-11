4eaca90d3576e17480f46a3cf9fd7717044f8133
monikadhayal <monikadhayal25@gmail.com>
2025-04-11 21:20:33 +0530

    delete todoltem

--- src/component/ShowTodo.jsx
+++ src/component/ShowTodo.jsx
@@ -1,5 +1,9 @@
-function ShowTodo({todoList}) {
-    console.log(todoList)
+import DeleteIcon from "@mui/icons-material/Delete";
+
+function ShowTodo(props) {
+  const {todoList,deleteTodoitem} = props;
+
+   console.log(todoList);
   return (
     <div>
       <div className="bg-white w-96 rounded-b-lg ">
@@ -7,8 +11,9 @@ function ShowTodo({todoList}) {
           {todoList.map((item) => (
             // <div className="bg-white text-black   px-4 py-2 rounded-lg border-l-[6px] border-l-blue-900 border-blue-900 w-80">
             // <div className="w-80 px-4 py-2 rounded-lg bg-white text-black border-l-4 border-l-blue-500 hover:border-l-pink-500 transition-all duration-300">
-             <div className="w-80 px-4 py-2 rounded-lg bg-white text-black border-l-[6px] border-l-blue-600 hover:shadow-[inset_6px_0_10px_#3b82f6] transition-all duration-500">
-              {item}
+            <div className="w-80 px-4 py-2 rounded-lg bg-white text-black border-l-[6px] border-l-blue-600 hover:shadow-[inset_6px_0_10px_#3b82f6] transition-all duration-500 flex justify-between" key={item.id}>
+              {item.todoItem}
+              <span onClick={() => {deleteTodoitem(item.id)}}> <DeleteIcon/> </span>
             </div>
           ))}
         </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import AddTodo from "../component/AddTodo";
 import Navbar from "../component/Navbar";
 import ShowTodo from "../component/ShowTodo";
 import { useState } from "react";
+import uuid4 from "uuid4";
 
 export default function HomePage() {
   const [count, setCount] = useState(0);
@@ -17,10 +18,16 @@ export default function HomePage() {
       alert("please enter something!");
       return;
     }
-    setTodoList([...todoList, todoItem]);
+    setTodoList([...todoList, {id: uuid4(),todoItem}]);
     setCount(count + 1);
     setTodoItem("");
   };
+  const deleteTodoitem = (id) => {
+        const updateList = todoList.filter((item) => {
+          return item.id !== id;
+        })
+        setTodoList(updateList);
+  }
    return (
      <div className="bg-blue-800 h-screen flex flex-col justify-center items-center">
        <Navbar />
@@ -30,7 +37,7 @@ export default function HomePage() {
        updateTodoList={updateTodoList} 
        todoItem={todoItem}
        />
-       <ShowTodo todoList={todoList} />
+       <ShowTodo todoList={todoList} deleteTodoitem={deleteTodoitem} />
      </div>
    );
  }
