d01d153c8f82cf58f5025fb19f05381a2fadf53b
monikadhayal <monikadhayal25@gmail.com>
2025-04-14 01:08:45 +0530

    Add Edit ficher

--- src/component/ShowTodo.jsx
+++ src/component/ShowTodo.jsx
@@ -1,9 +1,10 @@
 import DeleteIcon from "@mui/icons-material/Delete";
-
+import EditIcon from "@mui/icons-material/Edit";
 function ShowTodo(props) {
-  const {todoList,deleteTodoitem} = props;
+  const { todoList, deleteTodoitem, AllclearTodoList,handleEdit} = props;
 
-   console.log(todoList);
+  console.log(todoList);
+
   return (
     <div>
       <div className="bg-white w-96 rounded-b-lg ">
@@ -11,13 +12,30 @@ function ShowTodo(props) {
           {todoList.map((item) => (
             // <div className="bg-white text-black   px-4 py-2 rounded-lg border-l-[6px] border-l-blue-900 border-blue-900 w-80">
             // <div className="w-80 px-4 py-2 rounded-lg bg-white text-black border-l-4 border-l-blue-500 hover:border-l-pink-500 transition-all duration-300">
-            <div className="w-80 px-4 py-2 rounded-lg bg-white text-black border-l-[6px] border-l-blue-600 hover:shadow-[inset_6px_0_10px_#3b82f6] transition-all duration-500 flex justify-between" key={item.id}>
+            <div
+              className="w-80 px-4 py-2 rounded-lg bg-white text-black border-l-[6px] border-l-blue-600 hover:shadow-[inset_6px_0_10px_#3b82f6] transition-all duration-500 flex justify-between"
+              key={item.id}
+            >
               {item.todoItem}
-              <span onClick={() => {deleteTodoitem(item.id)}}> <DeleteIcon/> </span>
+
+              <span onClick={() => handleEdit(item)}>
+                <EditIcon />
+              </span>
+
+              <span
+                onClick={() => {
+                  deleteTodoitem(item.id);
+                }}
+              >
+                <DeleteIcon />
+              </span>
             </div>
           ))}
         </div>
-        <button className="bg-red-600 m-3 flext justify-center text-white w-20 h-8 rounded-md ">
+        <button
+          className="bg-red-600 m-3 flext justify-center text-white w-20 h-8 rounded-md "
+          onClick={AllclearTodoList}
+        >
           Clear All
         </button>
       </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,27 @@
 import AddTodo from "../component/AddTodo";
 import Navbar from "../component/Navbar";
 import ShowTodo from "../component/ShowTodo";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import uuid4 from "uuid4";
+
 
 export default function HomePage() {
   const [count, setCount] = useState(0);
   const [todoItem, setTodoItem] = useState("");
-  const [todoList, setTodoList] = useState([]);
+  const localStorageKey = "todoList";
+  const [editMode, setEditMode] = useState(false);
+  const [editId, setEditId] = useState(null);
+
+    
+  const [todoList, setTodoList] = useState(()=>{
+    return JSON.parse(localStorage.getItem(localStorageKey)) || [];
+    
+    });
+  
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(todoList));
+  }, [todoList]);
+
 
   const updateTodoList = (event) => {
     setTodoItem(event.target.value);
@@ -18,26 +32,58 @@ export default function HomePage() {
       alert("please enter something!");
       return;
     }
-    setTodoList([...todoList, {id: uuid4(),todoItem}]);
+    
+  if(editMode) {
+      const updatedTodos = todoList.map((item) => 
+      item.id === editId ? {...item, todoItem} : item);
+    
+    setTodoList(updatedTodos);
+    setEditMode(false);
+    setEditId(null);
+  } else {
+
+    setTodoList([...todoList, { id: uuid4(), todoItem }]);
     setCount(count + 1);
-    setTodoItem("");
-  };
-  const deleteTodoitem = (id) => {
-        const updateList = todoList.filter((item) => {
-          return item.id !== id;
-        })
-        setTodoList(updateList);
   }
-   return (
-     <div className="bg-blue-800 h-screen flex flex-col justify-center items-center">
-       <Navbar />
-       <AddTodo  
-       count={count}
-       AddTask={AddTask}
-       updateTodoList={updateTodoList} 
-       todoItem={todoItem}
-       />
-       <ShowTodo todoList={todoList} deleteTodoitem={deleteTodoitem} />
-     </div>
-   );
- }
+    setTodoItem("");
+    };
+
+  const deleteTodoitem = (id) => {
+    const updateList = todoList.filter((item) => {
+      return item.id !== id;
+    });
+    setCount(count - 1);
+    setTodoList(updateList);
+    };
+  const AllclearTodoList = () => {
+    setTodoList([]);
+    setCount(0);
+   };
+  
+  
+  const handleEdit = (item) => {
+    setTodoItem(item.todoItem); 
+    setEditMode(true);
+    setEditId(item.id);
+   };
+
+  
+
+  return (
+    <div className="bg-blue-800 h-screen flex flex-col justify-center items-center">
+      <Navbar />
+      <AddTodo
+        count={count}
+        AddTask={AddTask}
+        updateTodoList={updateTodoList}
+        todoItem={todoItem}
+      />
+      <ShowTodo
+        todoList={todoList}
+        deleteTodoitem={deleteTodoitem}
+        AllclearTodoList={AllclearTodoList}
+         handleEdit={handleEdit}
+      />
+    </div>
+  );
+}
